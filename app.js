cd4aa6532944dd8936685d459eb974afce478c85
edu <barajas.eduardo@uabc.edu.mx>
2019-06-20 08:21:42 -0700

    Connection fix

--- app.js
+++ app.js
@@ -56,5 +56,13 @@ app.use(function(err, req, res, next) {
     });
 });
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
 
 module.exports = app;
+
